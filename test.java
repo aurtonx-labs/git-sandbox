dd27e7f6b00647fa9eefcc63aa9bad1d00fe5dce
maurya-ax <maurya.aurtonx@gmail.com>
2026-09-12 12:47:24 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,12 @@
+public class ArithmeticExample {
+    public static void main(String[] args) {
+         Initialize variables
+         int a = 15;
+         int b = 4;
+
+        // // 1. Addition (+)
+        int sum = a + b;
+        System.out.println("Addition (" + a + " + " + b + "): " + sum);
+        return a + b;
+    }
+}
